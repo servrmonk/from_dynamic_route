1d2def97a4a6a8593748e044577d7b97058757d7
Raushan <roushankumarsingh2018@gmail.com>
2024-05-24 00:55:07 +0530

    post req and cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,8 @@
-const Product = require('../models/product');
-
+const Product = require("../models/product");
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product', //path will reamin same
-   
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product", //path will reamin same
   });
 };
 
@@ -15,26 +13,25 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
 };
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit //if thereis edit in the query parameter thani will catch other wise no, the extracted value is always is a string so true instead of true means false
-  if(!editMode){
-    return res.redirect('/')
+  const editMode = req.query.edit; //if thereis edit in the query parameter thani will catch other wise no, the extracted value is always is a string so true instead of true means false
+  if (!editMode) {
+    return res.redirect("/");
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product', //path will reamin same
-    editing:editMode, //check that true or not if true than add 
-   
+  res.render("admin/edit-product", {
+    pageTitle: "Edit Product",
+    path: "/admin/edit-product", //path will reamin same
+    editing: editMode, //check that true or not if true than add
   });
 };
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
 };
